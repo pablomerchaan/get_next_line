e1e933e9cda0703b77d3f71718642b2208e340c8
Rosario Arnau <rosario@iMac-de-Rosario.local>
2024-05-08 16:16:53 +0200

    2commit

--- get_next_line.c
+++ get_next_line.c
@@ -1,10 +1,37 @@
 
-static char	*read_file(int fd)
+static char	*ft_join(char *text, char *line)
 {
-	static char	*buffer;
-	char		*line;
+	char	*tmp;
 
-	
+	tmp = ft_strjoin(text, line);
+	free(text);
+	return(tmp);
+}
+
+static char	*read_file(int fd, char *text)
+{
+	char	*buffer;
+	int	byte_read;
+
+	if (!text)
+		text = ft_calloc(1, 1);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(buffer);
+			return(NULL);
+		}
+		buffer[byte_read] = '\0';
+		text = ft_join(text, buffer);
+		if (ft_strchr(buffer, '\n')
+			break;
+	}
+	free(buffer);
+	return(text);
 }
 
 static char	*next_line(int fd, char *buffer)
@@ -14,16 +41,13 @@ static char	*next_line(int fd, char *buffer)
 
 char	*get_next_line(int fd)
 {
-	int	bytes_read;
 	char	*line;
 	static char	*buffer;
 
-	line = get_line(buffer, fd);
-	buffer = ft_calloc(ft_strlen(line) + 1, sizeof(char));
-	if (buffer == NULL)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0 0) < 0)
 		return (NULL);
-	bytes_read = read(fd, buffer, ft_strlen(line));
-	if (bytes_read <= 0)
+	buffer = read_file(fd, buffer);
+	if (!buffer)
 		return (NULL);
 	
 
